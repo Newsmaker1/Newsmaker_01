8af8951074d59cfa307cced4276d59311b058952
Newsmaker1 <diaspora.bot.1@gmail.com>
2026-05-19 14:29:12 +0900

    Update models_import.py

--- database/models_import.py
+++ database/models_import.py
@@ -1,5 +1,28 @@
+from models.delivery import Delivery
+from models.destination import Destination
+from models.log import Log
+from models.post import Post
+from models.routing_rule import RoutingRule
+from models.setting import Setting
+from models.source_cache import SourceCache
+from models.source_pack import PackSource, SourcePack
+from models.subscription import Subscription
+from models.support_message import SupportMessage
 from models.user import User
+from models.user_pack_subscription import UserPackSubscription
 
 __all__ = [
     "User",
+    "Subscription",
+    "Destination",
+    "SourcePack",
+    "PackSource",
+    "UserPackSubscription",
+    "RoutingRule",
+    "Post",
+    "Delivery",
+    "SourceCache",
+    "SupportMessage",
+    "Setting",
+    "Log",
 ]
